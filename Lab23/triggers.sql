7ba060a12c5dae88fc3d783ddaf858be985f99fe
Cezary Januszek <cezary.januszek@epfl.ch>
2021-02-23 15:06:50 +0100

    Triggers done

--- Lab23/triggers.sql
+++ Lab23/triggers.sql
@@ -52,15 +52,47 @@ CREATE TRIGGER addRegistration
 CREATE OR REPLACE FUNCTION unregisterStudent () RETURNS TRIGGER AS $$
     DECLARE 
         studentStatus TEXT;
+        studentToInsert CHAR(10);
+        LastPosition INT;
+        currentPosition INT;
+        limitedCapacity INT;
+        studentCount INT;
     BEGIN
         studentStatus := (SELECT status FROM Registrations WHERE student=OLD.student AND course=OLD.course);
         IF (NOT EXISTS (SELECT student, course FROM Registrations WHERE student=OLD.student AND course=OLD.course)) THEN
             RAISE EXCEPTION 'ERROR: this student is not in the registrations for this course!';
 
-        ELSEIF (studentStatus = 'registered') THEN
+        ELSEIF (NOT EXISTS (SELECT code FROM LimitedCourses WHERE code=OLD.course) ) THEN --unregister from unlimited course
             DELETE FROM Registered WHERE student=OLD.student AND course=OLD.course;
-        ELSEIF (studentStatus = 'waiting') THEN
+
+        ELSEIF (studentStatus = 'registered') THEN
+            limitedCapacity := (SELECT capacity FROM LimitedCourses WHERE code=OLD.course);
+
+            DELETE FROM Registered WHERE student=OLD.student AND course=OLD.course;
+            studentCount := (SELECT COUNT(student) FROM Registered WHERE course=OLD.course); --count to check if not overfull
+
+            IF (EXISTS (SELECT course FROM WaitingList WHERE course=OLD.course) AND (studentCount<limitedCapacity)) THEN --limited with waiting list
+                studentToInsert := (SELECT student FROM WaitingList WHERE course=OLD.course AND position=1);
+                INSERT INTO Registered VALUES (studentToInsert,OLD.course);
+                --remove the student from waitinglist and update it
+                LastPosition:=(SELECT MAX(position) FROM WaitingList WHERE(course=OLD.course));
+                DELETE FROM WaitingList WHERE student=studentToInsert AND course=OLD.course;
+                IF(1!=LastPosition) THEN
+                    FOR counter IN 2..LastPosition loop
+                        UPDATE WaitingList SET position = counter-1 WHERE(course=OLD.course AND position=counter);
+                    END loop;
+                END IF;
+            END IF;
+
+        ELSEIF (studentStatus = 'waiting') THEN --remove from waiting list
+            currentPosition:=(SELECT position FROM WaitingList WHERE(student=OLD.student AND course=OLD.course));
+            LastPosition:=(SELECT MAX(position) FROM WaitingList WHERE(course=OLD.course));
             DELETE FROM WaitingList WHERE student=OLD.student AND course=OLD.course;
+            IF(currentPosition!=LastPosition) THEN
+                FOR counter IN currentPosition+1..LastPosition loop
+                    UPDATE WaitingList SET position = counter-1 WHERE(course=OLD.course AND position=counter);
+                END loop;
+            END IF;
         END IF;
         RETURN OLD;
     END;
